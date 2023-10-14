b784c6713c707cc687e27dc4c61847b4383ad38c
Ranie <143940800+ranyesantos@users.noreply.github.com>
2023-10-14 10:55:27 -0300

    adicão do materializecss

--- home.php
+++ home.php
@@ -20,10 +20,18 @@ mysqli_close($connect);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>olá <?php echo $data['nome'] ?></h1>
-    <a href="action/logout.php">Logout</a>
+    <div class="center-align" >
+        <h1>Olá, <?php echo $data['nome'] ?></h1>
+        <p class="flow-text">Infelizmente, ainda não há nada por aqui. Mas já estamos trabalhando nisso!</p>
+            <div class="row" >
+                <a class="btn-floating btn-small cyan pulse"><i class="material-icons">power_settings_new</i></a>
+                <a href="action/logout.php">Logout</a>
+            </div>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -36,26 +36,50 @@ if(isset($_POST['sub-button'])){
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Login</h1>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-<?php 
-if(!empty($errors)){
-    foreach($errors as $error){
-        echo $error;
-    }
-}
-?>
-    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-        <input type="text" placeholder="Usuário" name="user" id=""><br>
-        <input type="password" placeholder="Senha" name="password" id=""><br>
-        <button type="submit" name="sub-button">Entrar</button>
-    </form>
-</body>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <?php 
+         if(!empty($errors)){
+            foreach($errors as $error){
+                echo $error;
+            }
+        }
+        ?>
+    <div class="row">
+        <div class=" col s6 m12 center push-m5 row"> 
+            <form class="col  s12" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+                <br>
+                <br>
+                <div class="row">
+                    <div class=" card-action input-field col s2" >
+                    <i class="material-icons prefix">account_circle</i>
+                    <input id="icon_prefix" name="user" type="text" class="validate">
+                    <label for="icon_prefix">First Name</label>
+                    </div>
+                </div>
+
+                <div class="row" >
+                    <div class="input-field col s2" >
+                        <i class="material-icons prefix">https</i>
+                        <input id="pass_icon_prefix" name="password" type="password" class="validate">
+                        <label for="pass_icon_prefix">Senha</label>
+                        <button type="submit" class=" cyan waves-effect waves-light btn" name="sub-button">Entrar</button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+        <!--JavaScript at end of body for optimized loading-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
 </html>
+
